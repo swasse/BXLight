9abb7d256f086d3357a9a0962585a4d97359954d
David <david.van.steertegem@gmail.com>
2019-03-22 09:11:11 +0100

    added synchronize function to download updates

--- app/src/main/java/be/ehb/bxlight/MainActivity.java
+++ app/src/main/java/be/ehb/bxlight/MainActivity.java
@@ -1,13 +1,18 @@
 package be.ehb.bxlight;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import be.ehb.bxlight.app_loading.LoadScreenActivity;
 import be.ehb.bxlight.fragments.AboutFragment;
 import be.ehb.bxlight.fragments.ComicListFragment;
 import be.ehb.bxlight.fragments.MapFragment;
@@ -53,6 +58,24 @@ public class MainActivity extends AppCompatActivity {
 
         MapFragment mf = MapFragment.newInstance();
         switchFragment(mf);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        sp.edit().putBoolean("downloaded", false).apply();
+
+        Intent i = new Intent(getApplicationContext(), LoadScreenActivity.class);
+        startActivity(i);
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onBackPressed() {
